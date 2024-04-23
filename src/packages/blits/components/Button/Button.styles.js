e33f2114c1bce291ee316ee4729ea92e16b5dedd
Matt Wooding <44930483+WoodingMP@users.noreply.github.com>
2024-04-23 12:23:19 +0100

    wip(blits): js button component + story

---
+++ src/packages/blits/components/Button/Button.styles.js
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import theme from '../../../l3-ui-theme-base/theme.js';
+import { makeComponentStyles } from '../../../solid/utils/getThemeStyles.js';
+
+/* @ts-expect-error next-line themes are supplied by client applications so this setup is necessary */
+const { Button: { defaultTone, ...themeStyles } = { styles: {} } } = theme.componentConfig;
+/* @ts-expect-error next-line see above */
+const { Surface: { surfaceDefaultTone, ...surfaceThemeStyles } = { styles: {} } } = theme.componentConfig;
+
+const container = {
+  themeKeys: {
+    textAlign: 'textAlign',
+    borderRadius: 'radius',
+    color: 'backgroundColor',
+    justifyContent: 'justifyContent'
+  },
+  base: {
+    height: theme.typography.button1.lineHeight + theme.spacer.xl * 2,
+    display: 'flex',
+    padding: [theme.spacer.xxxl, theme.spacer.xl],
+    color: theme.color.interactiveNeutral,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: theme.radius.sm,
+    contentColor: theme.color.fillNeutral
+  },
+  modes: {
+    focus: {
+      color: theme.color.interactiveNeutralFocus,
+      contentColor: theme.color.fillInverse
+    },
+    disabled: {
+      color: theme.color.fillNeutralDisabled
+    }
+  },
+  tones: {
+    inverse: {
+      color: theme.color.interactiveInverse
+    },
+    brand: {
+      color: theme.color.interactiveBrand,
+      focus: {
+        color: theme.color.fillNeutral
+      }
+    }
+  },
+  themeStyles: {
+    ...surfaceThemeStyles,
+    ...themeStyles
+  }
+};
+
+const content = {
+  themeKeys: {
+    color: 'contentColor'
+  },
+  base: {
+    color: theme.color.textNeutral
+  },
+  modes: {
+    focus: {
+      color: theme.color.textInverse
+    },
+    disabled: {
+      color: theme.color.textNeutralDisabled
+    }
+  },
+  tones: {
+    inverse: {
+      color: theme.color.fillNeutral
+    },
+    brand: {
+      color: theme.color.fillBrand,
+      focus: {
+        color: theme.color.fillBrand
+      }
+    }
+  },
+  themeStyles: {
+    ...surfaceThemeStyles,
+    ...themeStyles
+  }
+};
+
+const text = {
+  themeKeys: {
+    color: 'textColor'
+  },
+  base: {
+    textAlign: 'left',
+    color: theme.color.textNeutral,
+    ...theme.typography.button1
+  },
+  modes: {
+    focus: {
+      color: theme.color.textInverse
+    },
+    disabled: {
+      color: theme.color.textNeutralDisabled
+    }
+  },
+  tones: {
+    inverse: {
+      color: theme.color.fillNeutral
+    },
+    brand: {
+      color: theme.color.fillBrand,
+      focus: {
+        color: theme.color.fillBrand
+      }
+    }
+  },
+  themeStyles: {
+    ...surfaceThemeStyles,
+    ...themeStyles
+  }
+};
+
+const Container = makeComponentStyles(container);
+const Content = makeComponentStyles(content);
+const Text = makeComponentStyles(text);
+
+const styles = {
+  tone: defaultTone || surfaceDefaultTone || 'neutral',
+  Container,
+  Content,
+  Text
+};
+
+export default styles;
